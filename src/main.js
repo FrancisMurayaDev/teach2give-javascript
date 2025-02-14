0d325e99e56430d5252081fd5767c453aed03aa3
FrancisMurayaDev <francismurayadev@gmail.com>
2025-02-14 12:10:49 +0300

    add concantination notes

--- src/main.js
+++ src/main.js
@@ -75,3 +75,152 @@ console.log(x); // 9
 
 let y = 10;
 console.log(--y); // 9 (increments first, then returns)
+
+//Operators
+
+let age = 34;
+//Adding numbers.
+let x = 34;
+let y = 26;
+console.log(x + y); // 60
+
+//Adding Strings
+let firstName = "Joe";
+let lastName = "Smith";
+console.log(firstName + lastName); // JoeSmith
+
+//Subtracting numbers.
+let x = 36;
+let y = 24;
+console.log(x - y); // 12
+
+//Multiplying numbers.
+let x = 3;
+let y = 2;
+console.log(x * y); // 6
+
+//Dividing numbers.
+let x = 6;
+let y = 2;
+console.log(6 / 2); // 3
+
+//Modulus
+let x = 6;
+let y = 2;
+console.log(6 % 2); // 0
+
+//Exponentiation
+let x = 10;
+let y = 4;
+console.log(10 ** 4); // 10000
+
+let x = 10;
+console.log(x++); // 10 (returns first, then increments)
+console.log(x); // 11
+
+let y = 10;
+console.log(++y); // 11 (increments first, then returns)
+
+let x = 10;
+console.log(x--); // 10 (returns first, then increments)
+console.log(x); // 9
+
+let y = 10;
+console.log(--y); // 9 (increments first, then returns)
+
+//Assignment Operators
+
+let age = 34;
+let age = 5;
+age += 4 // age = 5 = 4;
+
+let age = 15;
+age -=5; // age = 15 -5 = 10
+
+let x = 5;
+x *= 2; // x = x * 2 → 10
+
+let x = 15;
+x /= 3; // x = x / 3 → 5  
+
+//Comparision Operators
+
+let a = 10;
+let b = 5;
+console.log(a == b); // false
+console.log(a == 10); // true
+
+console.log(7 == "7"); // true
+
+let a = 10;
+let b = 5;
+console.log(a === b); // false
+console.log(a === 10); // true
+
+console.log(7 === "7"); // false
+
+let a = 7;
+let b = 3;
+console.log(a != b); // true
+console.log(a != 7); // false
+
+console.log(7 != "7"); // false
+
+let a = 10;
+let b = 5;
+console.log(a !== b); // true
+console.log(a !== 10); // false
+
+console.log(10 !== "10"); // true
+
+let a = 6;
+let b = 2;
+console.log(a > b); // true
+console.log(b > a); // false
+
+let a = 7;
+let b = 5;
+console.log(a < b); // false
+console.log(b < a); // true
+
+let a = 10;
+let b = 5;
+console.log(a >= b); // true
+console.log(b >= a); // false
+console.log(10 >= 10); // true
+
+let a = 6;
+let b = 3;
+console.log(a <= b); // false
+console.log(b <= a); // true
+console.log(7 <= 7); // true
+
+console.log(true && true); // true
+console.log(true && false); // false
+
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || false); // false
+
+console.log(!true); // false
+console.log(!false); // true
+
+// Concantination 
+
+let firstName = "Francis";
+let lastName = "Muraya";
+let fullName = firstName + lastName;
+console.log("My full name is " + fullName);
+
+let message = "Hello ";
+message += "Francis.";
+console.log(message);
+
+let firstName = "Francis";
+let lastName = "Muraya";
+console.log(`My name is ${firstName} ${lastName}`);
+
+
+
+
+
